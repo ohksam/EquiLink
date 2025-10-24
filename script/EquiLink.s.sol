0b0c4ac61254a242c56b629103c486a963de4240
Sam Oh <SamOh37@gmail.com>
2025-10-23 21:48:44 -0400

    update pricefeeds in script

--- script/EquiLink.s.sol
+++ script/EquiLink.s.sol
@@ -7,9 +7,11 @@ import {EquiLink} from "../src/EquiLink.sol";
 import "forge-std/console.sol";
 
 contract DeployEquiLink is Script {
-    address constant ETH_USD_FEED = 0x694AA1769357215DE4FAC081bf1f309aDC325306; // Sepolia
-    address constant BTC_USD_FEED = 0x5741306c21795FdCBb9b265Ea0255F499DFe515C; // Sepolia
-    address constant LINK_USD_FEED = 0xd9FFdb71EbE7496cC440152d43986Aae0AB76665; // Sepolia
+    // Sepolia addresses
+    address constant ETH_USD_FEED = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+    address constant BTC_USD_FEED = 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43;
+    address constant LINK_USD_FEED = 0xc59E3633BAAC79493d908e63626716e204A45EdF;
+
     
     function run() external {
         vm.startBroadcast();
